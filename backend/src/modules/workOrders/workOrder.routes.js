9ae31a841f26a0cfddff08d98329a12ee8057e25
mencariberkah <mencariberkah807@gmail.com>
2026-09-15 12:13:04 +0700

    Add work order routes

---
+++ backend/src/modules/workOrders/workOrder.routes.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import * as controller from './workOrder.controller.js';
+
+const router = Router();
+router.get('/:id', controller.getById);
+router.post('/:id/start', controller.start);
+router.post('/:id/complete', controller.complete);
+
+export default router;

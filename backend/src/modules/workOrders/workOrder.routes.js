cc027a086928ea4ad4604f4bb1ee42bcf3e6b900
mencariberkah <mencariberkah807@gmail.com>
2026-09-15 12:15:53 +0700

    Register work order list route

--- backend/src/modules/workOrders/workOrder.routes.js
+++ backend/src/modules/workOrders/workOrder.routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import * as controller from './workOrder.controller.js';
 
 const router = Router();
+router.get('/', controller.list);
 router.get('/:id', controller.getById);
 router.post('/:id/start', controller.start);
 router.post('/:id/complete', controller.complete);
